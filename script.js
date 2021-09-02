c8a826a4e6cba15607598392d2ce3bcbbf5dc477
phoenixwright-creator <phoenixwright725@gmail.com>
2021-09-02 20:14:39 +0200

    Modification du bouton afin de créer un nouveau panneau de taille differente, l'utilisateur entre une taille au choix limitee a 100x100 carreaux + epuration des couleurs a l'ecran pour un meilleur rendu

--- script.js
+++ script.js
@@ -1,46 +1,81 @@
 const squareDiv = document.createElement('div');
 const container = document.getElementById('container');
+const btnDiv = document.createElement('div');
 const button = document.createElement('button');
 let windowHeight = window.innerHeight - 50;
 let windowWidth = window.innerWidth;
 
 document.body.style.margin = "0px";
-document.body.style.marginTop = "50px";
+document.body.style.marginTop = "0px";
 document.body.style.padding = "0px";
 
+container.style.height = windowHeight;
+container.style.width = windowWidth;
 container.style.display = "grid";
-container.style.gridGap = "0px";
+container.style.gridGap = "1px";
+container.style.justifyContent = "center";
+container.style.paddingTop = "30px";
+container.style.paddingBottom = "30px";
 
-document.body.insertBefore(button, container);
-button.textContent = "Clear the panel";
-button.id = "clearButton";
+document.body.insertBefore(btnDiv, container);
+btnDiv.appendChild(button);
+button.textContent = "Click to draw a new panel";
+button.id = "drawButton";
 
+btnDiv.style.height = "100px";
+btnDiv.style.display = "flex";
+btnDiv.style.justifyContent = "center";
+btnDiv.style.alignItems = "center";
+btnDiv.style.borderBottom = "2px solid black";
 
-for(let j=1; j<=windowHeight/16; j++){
+button.style.height = "50px";
+button.style.width = "200px";
+
+function constructPanel(sizeGrid) {
+
+    for(let j=1; j<=sizeGrid; j++){
     
-    for(let i=1; i<=windowWidth/16; i++){
-        const squareDiv = document.createElement('div');
-        squareDiv.className = "box16";
-        squareDiv.style.backgroundColor = "green";
-        squareDiv.style.height = "16px";
-        squareDiv.style.width = "16px";
-        squareDiv.style.gridColumn = `${i} / ${i+1}`;
-        container.appendChild(squareDiv);
+        for(let i=1; i<=sizeGrid; i++){
+            const squareDiv = document.createElement('div');
+            squareDiv.className = "box16";
+            squareDiv.style.backgroundColor = "green";
+            squareDiv.style.height = "16px";
+            squareDiv.style.width = "16px";
+            squareDiv.style.gridColumn = `${i} / ${i+1}`;
+            container.appendChild(squareDiv);
+        }
+    
+    }
+
+    const boxes = document.querySelectorAll('.box16');
+
+    for(let k = 0; k<boxes.length; k++) {
+
+        boxes[k].addEventListener('mouseover', () => {
+
+            boxes[k].style.backgroundColor = "red";
+
+        });
+
     }
 
 }
 
-const boxes = document.querySelectorAll('.box16');
+constructPanel(30);
 
+document.getElementById("drawButton").addEventListener('click', () => {
+    while (container.hasChildNodes()) {  
 
-for(let counter = 0; counter<boxes.length; counter++) {
-    boxes[counter].addEventListener('mouseover', () => {
-        boxes[counter].style.backgroundColor = "red";
-    });
-}
+        container.removeChild(container.firstChild);
 
-document.getElementById("clearButton").addEventListener('click', () => {
-    for(let counter = 0; counter<boxes.length; counter++) {
-        boxes[counter].style.backgroundColor = "green";
     }
-})
+
+    let sizeGrid = 200;
+
+    while(sizeGrid>100){
+        sizeGrid = parseInt(prompt('Please enter the size of the grid you want to draw on (limited to 100px) : '));
+    }
+    
+    constructPanel(sizeGrid);
+
+});
